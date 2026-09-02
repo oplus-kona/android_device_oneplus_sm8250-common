051cb1382a4f1253154546288f91a9a2039f51fa
zenzer0s <praveenzalaki200@gmail.com>
2026-09-02 18:32:44 +0000

    sm8250-common: Convert legacy overlays to RRO
    Change-Id: Icf18e1a590e4267835df74213005128bcdac1be1

--- common.mk
+++ common.mk
@@ -247,14 +247,14 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    LineageSDKResCommon \
+    LineageSettingsProviderResCommon \
     NcmTetheringOverlay \
+    OplusDozeResCommon \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
