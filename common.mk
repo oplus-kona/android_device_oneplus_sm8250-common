70085472f9d041b1a4d2198fa575e968e2d5d927
zenzer0s <praveenzalaki200@gmail.com>
2026-09-03 03:40:34 +0000

    sm8250-common: Inherit Dolby from hardware/dolby
    Change-Id: I21cf4aefb304abf851b74d6f4c925a542d207c44

--- common.mk
+++ common.mk
@@ -131,9 +131,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/advanced_sf_offsets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/advanced_sf_offsets.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-# Dolby Atmos & Vision
-TARGET_SUPPORTS_DOVI := true
-$(call inherit-product, vendor/dolby/dolby-setup.mk)
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
