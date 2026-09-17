2aebe6e79752dfc3098630617427d9e91c4f5bba
zenzer0s <praveenzalaki200@gmail.com>
2026-09-17 05:40:57 +0000

    sm8250-common: Migrate libcameraservice ext to soong_config_set

--- common.mk
+++ common.mk
@@ -107,6 +107,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# CameraService Extension
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.sm8250)
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
