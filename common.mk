8069a8f1276da367898367915d6aafea40897539
zenzer0s <praveenzalaki200@gmail.com>
2026-09-17 06:38:24 +0000

    sm8250-common: Switch to common QTI Telephony component
    - Add telephony to TARGET_COMMON_QTI_COMPONENTS
    - Remove redundant in-tree telephony packages and permission copy rules
    - Remove prebuilt QtiTelephonyService, qcrilhook, and qcrilmsgtunnel

--- common.mk
+++ common.mk
@@ -208,6 +208,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     init \
     overlay \
     perf \
+    telephony \
     wfd \
     wlan
 
@@ -322,29 +323,12 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper.xml \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti-telephony-utils-prd \
-    qti_telephony_utils.xml \
-    qti_telephony_utils_prd.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
